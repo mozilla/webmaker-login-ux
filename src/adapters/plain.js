1bb1f1c92adb4c486ba2969e9d309263b3b4e079
Jon Buckley <jon@jbuckley.ca>
2014-10-09 17:28:04 -0400

    more working

--- src/adapters/plain.js
+++ src/adapters/plain.js
@@ -25,17 +25,70 @@ var WebmakerLogin = function WebmakerLogin(options) {
 WebmakerLogin.prototype.create = function() {
   var controller = this.wmLogin.joinWebmaker();
   var scope = {
-    currentState: 0,
     MODALSTATE: {
       inputEmail: 0,
       inputUsername: 1,
       welcome: 2
+    },
+    currentState: 0,
+    form: {
+      user: {
+        $error: {}
+      }
+    },
+    user: {},
+    sendingRequest: false,
+    welcomeModalIdx: Math.floor(Math.random() * 4),
+    canSubmitEmail: function() {
+      return scope.user.email && scope.user.agree;
     }
   };
+  var modal_fragment = _create_modal_fragment(ui.create);
 
-  var modal = _create_modal(ui.create);
-  _run_expressions(modal, scope);
-  _open_modal(modal);
+  controller.on('sendingRequest', function (state) {
+    scope.sendingRequest = state;
+    _run_expressions(modal, scope);
+  });
+
+  controller.on('displayEmailInput', function () {
+    scope.currentState = scope.MODALSTATE.inputEmail;
+    _run_expressions(modal, scope);
+  });
+
+  controller.on('displayUsernameInput', function () {
+    scope.currentState = scope.MODALSTATE.inputUsername;
+    _run_expressions(modal, scope);
+  });
+
+  controller.on('displayWelcome', function (user) {
+    scope.currentState = scope.MODALSTATE.welcome;
+    _run_expressions(modal, scope);
+  });
+
+  controller.on('displayAlert', function (alertId) {
+    console.log(alertId);
+    scope.form.user.$error[alertId] = true;
+    _run_expressions(modal, scope);
+  });
+
+  controller.on('hideAlert', function (alertId) {
+    scope.form.user.$error[alertId] = false;
+    _run_expressions(modal, scope);
+  });
+
+  modal_fragment.querySelector('input[name="email"]').addEventListener('blur', function(e) {
+    scope.user.email = e.target.value;
+    controller.validateEmail(scope.user.email);
+  });
+
+  modal_fragment.querySelector('input[name="agree"]').addEventListener('change', function(e) {
+    scope.user.agree = e.target.checked;
+    _run_expressions(modal, scope);
+  });
+
+  _run_expressions(modal_fragment, scope);
+  _open_modal(modal_fragment);
+  var modal = document.querySelector('body > div.modal');
 };
 
 WebmakerLogin.prototype.login = function() {
@@ -46,18 +99,26 @@ WebmakerLogin.prototype.logout = function() {
 
 };
 
-var _create_modal = function(template) {
+var _create_modal_fragment = function(template) {
   var range = document.createRange();
   range.selectNode(document.body);
-  var modal = range.createContextualFragment(ui.wrapper);
-  modal.querySelector('.modal-content').appendChild(range.createContextualFragment(template));
+  var modal_fragment = range.createContextualFragment(ui.wrapper);
+  modal_fragment.querySelector('.modal-content').appendChild(range.createContextualFragment(template));
 
-  return modal;
+  return modal_fragment;
 };
 
 var _run_expressions = function(modal, scope) {
-  var elements = modal.querySelectorAll('[ng-hide],[ng-show]');
+  var elements = modal.querySelectorAll('[ng-hide],[ng-show],[ng-disabled]');
   for (var i = 0; i < elements.length; i++) {
+    if (elements[i].getAttribute('ng-disabled')) {
+      if (expressions.compile(elements[i].getAttribute('ng-disabled'))(scope)) {
+        elements[i].setAttribute('disabled', true);
+      } else {
+        elements[i].removeAttribute('disabled');
+      }
+    }
+
     if (elements[i].getAttribute('ng-hide')) {
       if (expressions.compile(elements[i].getAttribute('ng-hide'))(scope)) {
         elements[i].classList.add('hide');
@@ -76,8 +137,13 @@ var _run_expressions = function(modal, scope) {
   }
 };
 
-var _open_modal = function(modal) {
-  document.body.appendChild(modal);
+var _open_modal = function(modal_fragment) {
+  document.body.appendChild(modal_fragment);
+};
+
+var _close_modal = function() {
+  document.body.removeChild(document.querySelector('body > div.modal-backdrop'));
+  document.body.removeChild(document.querySelector('body > div.modal'));
 };
 
 window.WebmakerLogin = WebmakerLogin;
