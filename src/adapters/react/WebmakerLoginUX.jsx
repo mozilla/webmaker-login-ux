081455a2382103044ec0585a46d813b2983c32c1
Pomax <pomax@nihongoresources.com>
2015-01-26 13:51:07 -0800

    react component

---
+++ src/adapters/react/WebmakerLoginUX.jsx
@@ -0,0 +1,28 @@
+var React = require("react");
+var webmakerLoginUxMixin = require("./WebmakerUXMixin");
+
+var WebmakerLoginUX = React.createClass({
+
+  mixins: [
+    webmakerLoginUxMixin
+  ],
+
+  render: function() {
+    var handler = this.login,
+        label = this.props.signInLabel;
+
+    if (this.props.loggedIn) {
+      handler = this.logout;
+      label = this.props.signOutLabel;
+    }
+
+    return (
+      <button
+        className="btn btn-primary webmaker-login-ux"
+        onClick={handler}>{label}</button>
+    );
+  }
+
+});
+
+module.exports = WebmakerLoginUX;
