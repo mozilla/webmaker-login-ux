2577951391d94a44c528f4d3a150b937348a7d93
Pomax <pomax@nihongoresources.com>
2015-01-26 14:43:47 -0800

    post-transform

--- src/adapters/react/WebmakerLoginUX.js
+++ src/adapters/react/WebmakerLoginUX.js
@@ -7,20 +7,21 @@ var WebmakerLoginUX = React.createClass({
     webmakerLoginUxMixin
   ],
 
-  render: function() {
+  render: function () {
     var handler = this.login,
-        label = this.props.signInLabel;
+      label = this.props.signInLabel;
 
     if (this.props.loggedIn) {
       handler = this.logout;
       label = this.props.signOutLabel;
     }
 
-    return (
-      <button
-        className="btn btn-primary webmaker-login-ux"
-        onClick={handler}>{label}</button>
-    );
+    var button = React.DOM.button({
+      className: "btn btn-primary webmaker-login-ux",
+      onClick: handler
+    }, label);
+
+    return button;
   }
 
 });
